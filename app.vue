a9c2e947d593c1383b53d503a5d75cdb8debc43c
0PandaDEV <github.com@pandadev.net>
2025-04-03 19:37:22 +0200

    feat: create main app layout with global styles and custom fonts

---
+++ app.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+:root {
+  --background: #191919;
+  --element: #2b2b2b;
+  --border: #ffffff1a;
+
+  --text: #e6e6e6;
+  --text-secondary: #a6a6a6;
+}
+
+@font-face {
+  font-family: DepartureMono;
+  src: url("/fonts/DepartureMono.woff2") format("woff2");
+  font-display: swap;
+  font-weight: normal;
+  font-style: normal;
+  font-weight: 100 800;
+}
+
+@font-face {
+  font-family: Outfit;
+  src: url("/fonts/Outfit.woff2") format("woff2");
+  font-display: swap;
+  font-weight: normal;
+  font-style: normal;
+  font-weight: 100 900;
+}
+
+@font-face {
+  font-family: Overt;
+  src: url("/fonts/Overt.woff2") format("woff2");
+  font-display: swap;
+  font-weight: normal;
+  font-style: normal;
+  font-weight: 400 500;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: var(--background);
+  color: var(--text);
+  font-size: 14px;
+  font-family: Outfit;
+}
+
+button,
+input {
+  font-family: Outfit;
+  font-size: 14px;
+}
+
+a{
+  text-decoration: none;
+  color: var(--text);
+}
+</style>
