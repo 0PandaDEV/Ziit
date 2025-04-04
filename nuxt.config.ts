c1f0592ede9f637f9477f7e84e0774c0878545b7
0PandaDEV <github.com@pandadev.net>
2025-04-04 22:33:03 +0200

    feat: configure app metadata with charset, viewport, and title in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,11 @@ export default defineNuxtConfig({
   nitro: {
     preset: "bun",
   },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+      title: "Ziit",
+    }
+  }
 });
