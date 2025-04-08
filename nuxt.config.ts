3f65091a1ef0ddb93ec77c226e2c51a87d6b184e
0PandaDEV <github.com@pandadev.net>
2025-04-08 22:11:35 +0200

    feat: add security-related meta tags in nuxt.config.ts for enhanced application security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,41 @@ export default defineNuxtConfig({
       viewport:
         "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
       title: "Ziit",
+      meta: [
+        {
+          "http-equiv": "Permissions-Policy",
+          content: "camera=(), microphone=(), geolocation=()",
+        },
+        { "http-equiv": "X-Frame-Options", content: "DENY" },
+        {
+          "http-equiv": "Clear-Site-Data",
+          content: '"cache","cookies","storage"',
+        },
+        {
+          "http-equiv": "Cross-Origin-Embedder-Policy",
+          content: "require-corp",
+        },
+        {
+          "http-equiv": "Strict-Transport-Security",
+          content: "max-age=31536000; includeSubDomains",
+        },
+        { "http-equiv": "X-Content-Type-Options", content: "nosniff" },
+        { "http-equiv": "X-Permitted-Cross-Domain-Policies", content: "none" },
+        {
+          "http-equiv": "Referrer-Policy",
+          content: "strict-origin-when-cross-origin",
+        },
+        { "http-equiv": "Cross-Origin-Opener-Policy", content: "same-origin" },
+        {
+          "http-equiv": "Cross-Origin-Resource-Policy",
+          content: "same-origin",
+        },
+        {
+          "http-equiv": "Content-Security-Policy",
+          content:
+            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'",
+        },
+      ],
     },
   },
 });
