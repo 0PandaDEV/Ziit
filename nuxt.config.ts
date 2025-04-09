d3b6cb2d669f73423dc7482b503da9f2aebe3ab9
0PandaDEV <github.com@pandadev.net>
2025-04-09 11:22:14 +0200

    refactor: remove unnecessary meta tags from nuxt.config.ts for cleaner configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,36 +18,6 @@ export default defineNuxtConfig({
       viewport:
         "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
       title: "Ziit",
-      meta: [
-        {
-          "http-equiv": "Permissions-Policy",
-          content: "camera=(), microphone=(), geolocation=()",
-        },
-        { "http-equiv": "X-Frame-Options", content: "DENY" },
-        {
-          "http-equiv": "Clear-Site-Data",
-          content: '"cache","cookies","storage"',
-        },
-        {
-          "http-equiv": "Cross-Origin-Embedder-Policy",
-          content: "require-corp",
-        },
-        {
-          "http-equiv": "Strict-Transport-Security",
-          content: "max-age=31536000; includeSubDomains",
-        },
-        { "http-equiv": "X-Content-Type-Options", content: "nosniff" },
-        { "http-equiv": "X-Permitted-Cross-Domain-Policies", content: "none" },
-        {
-          "http-equiv": "Referrer-Policy",
-          content: "strict-origin-when-cross-origin",
-        },
-        { "http-equiv": "Cross-Origin-Opener-Policy", content: "same-origin" },
-        {
-          "http-equiv": "Cross-Origin-Resource-Policy",
-          content: "same-origin",
-        },
-      ],
     },
   },
 });
